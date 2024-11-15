2172a4a68c029af231a3468bdc57842960ec1aab
Steven Pescheteau <steven.pescheteau@gmail.com>
2024-11-15 18:28:14 +0100

    Config for webhook dedicated to internal purpose has been added

--- models/constants/config.go
+++ models/constants/config.go
@@ -22,6 +22,12 @@ const (
 	// RabbitMQ address.
 	RabbitMQAddress = "RABBITMQ_ADDRESS"
 
+	// Webhook ID used for internal purposes.
+	InternalWebhookID = "INTERNAL_WEBHOOK_ID"
+
+	// Webhook Token used for internal purposes.
+	InternalWebhookToken = "INTERNAL_WEBHOOK_TOKEN"
+
 	// Metric port.
 	MetricPort = "METRIC_PORT"
 
@@ -31,25 +37,29 @@ const (
 	// Boolean; used to register commands at development guild level or globally.
 	Production = "PRODUCTION"
 
-	defaultMySQLURL        = "localhost:3306"
-	defaultMySQLUser       = ""
-	defaultMySQLPassword   = ""
-	defaultMySQLDatabase   = "kaellybot"
-	defaultRabbitMQAddress = "amqp://localhost:5672"
-	defaultMetricPort      = 2112
-	defaultLogLevel        = zerolog.InfoLevel
-	defaultProduction      = false
+	defaultMySQLURL             = "localhost:3306"
+	defaultMySQLUser            = ""
+	defaultMySQLPassword        = ""
+	defaultMySQLDatabase        = "kaellybot"
+	defaultRabbitMQAddress      = "amqp://localhost:5672"
+	defaultInternalWebhookID    = ""
+	defaultInternalWebhookToken = ""
+	defaultMetricPort           = 2112
+	defaultLogLevel             = zerolog.InfoLevel
+	defaultProduction           = false
 )
 
 func GetDefaultConfigValues() map[string]any {
 	return map[string]any{
-		MySQLURL:        defaultMySQLURL,
-		MySQLUser:       defaultMySQLUser,
-		MySQLPassword:   defaultMySQLPassword,
-		MySQLDatabase:   defaultMySQLDatabase,
-		RabbitMQAddress: defaultRabbitMQAddress,
-		MetricPort:      defaultMetricPort,
-		LogLevel:        defaultLogLevel.String(),
-		Production:      defaultProduction,
+		MySQLURL:             defaultMySQLURL,
+		MySQLUser:            defaultMySQLUser,
+		MySQLPassword:        defaultMySQLPassword,
+		MySQLDatabase:        defaultMySQLDatabase,
+		RabbitMQAddress:      defaultRabbitMQAddress,
+		InternalWebhookID:    defaultInternalWebhookID,
+		InternalWebhookToken: defaultInternalWebhookToken,
+		MetricPort:           defaultMetricPort,
+		LogLevel:             defaultLogLevel.String(),
+		Production:           defaultProduction,
 	}
 }
